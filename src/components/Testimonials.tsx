e14c1b6cabe6aeaddccdf212c61b31ad0f818019
poornesh-39 <poorneshhsgowda2004@gmail.com>
2026-01-26 23:21:56 +0530

    Update Testimonials UI for Mobile screen

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -34,20 +34,35 @@ export default function Testimonials() {
   }, [])
 
   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 250,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
-    arrows: true,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 2, arrows: false } },
-      { breakpoint: 640, settings: { slidesToShow: 1, arrows: false } }
-    ]
-  }
+  dots: true,
+  infinite: true,
+  speed: 250,
+  slidesToShow: 3, 
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+
+  ]
+}
+
 
   return (
     <>
